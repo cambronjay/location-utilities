49afbb11a333a3799a76b1661439e7a3d42bde3d
Cambron <N106586@lgeenergy.int>
2017-05-16 11:04:44 -0400

    added readme to build

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -1,10 +1,16 @@
 var fs = require('fs');
 
 var packageJson = require('../package.json');
+var readme = require('../README.md');
+
 if (! packageJson) {
   throw new Error('Can\'t find package json');
+}
+if (! readme) {
+  throw new Error('Can\'t find readme');
 }
 
 delete packageJson.devDependencies;
 
 fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFile('./dist/README.md', JSON.stringify(readme, null, 2));
